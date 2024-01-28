7c4a28c7d44b70ee36962c08dfd419e68260fddd
Jumanazar04 <yusupovjumanazar004@gmail.com>
2024-01-28 12:49:35 +0500

    all right

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const array = [ [1,2], [true, false], 0, 'name', 'age'];
 
-array.forEach((user, i) =>{
-    if(typeof user == 'string'){
+// array.forEach((user, i) =>{
+//     if(typeof user == 'string'){
+//         console.log('String indexi:', i);
+//     }
+//     // console.log(typeof user);
+// })
+// // console.log(array);
+
+for (let i = 0; i < array.length; i++) {
+    if(typeof array[i] == 'string'){
         console.log('String indexi:', i);
-    }
-    // console.log(typeof user);
-})
-// console.log(array);
+    }    
+}
