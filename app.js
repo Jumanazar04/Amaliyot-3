0e37a75c687ca5342d7fcf5ed2f60f8403a362f1
Jumanazar04 <yusupovjumanazar004@gmail.com>
2024-01-30 16:21:43 +0500

    seccesfly

--- app.js
+++ app.js
@@ -121,3 +121,55 @@
 // }
 
 // console.log(result);
+
+
+// let num = 7;
+// let result = 0;
+
+// for(let i=1; i<=10; i++){
+//     result = num * i + 1;
+//     console.log(result);
+// }
+
+// let array = [1, 2, 3, -4, 5, 6];
+// let result = [];
+
+// for(let i = 0; i < array.length; i++){
+//     result.push(array[i] * -1);
+// }
+
+// console.log(result);
+
+let array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
+let result = [];
+
+// for (let i = 0; i < array.length; i++) {
+//     if(Math.round(array[i] % 3) == 0 ){
+//         result.push(  'fizz' );
+//     }else if (Math.round(array[i] % 5) == 0 ){
+//         result.push('buzz');
+//     }else if (Math.round(array[i] % 15) == 0 ){
+//         result.push('fizzbuzz');
+//     }else {
+//         result.push(array[i])
+//     }
+    
+    
+    
+    
+// }
+// console.log(result);
+
+array.forEach((item) =>{
+    if(Math.round(item % 3) == 0 && Math.round(item % 5) != 0 ){
+        result.push('fizz');
+    }else if (Math.round(item % 5) == 0 && Math.round(item % 3) != 0 ){
+        result.push('buzz');
+    }else if (Math.round(item % 5) == 0 || Math.round(item % 3) == 0 ){
+        result.push('fizzbuzz');
+    }else {
+        result.push(item)
+    }
+})
+
+console.log(result);
